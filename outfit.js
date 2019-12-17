b5434b096f79d287046a965f4916d01e56ac8612
Leah Winters <leah.winters24@gmail.com>
2019-12-17 15:55:16 -0700

    :art: Create a class for outfit

---
+++ outfit.js
@@ -0,0 +1,9 @@
+class Outfit {
+  constructor(title, id, background) {
+    this.title = title;
+    this.id = id;
+    this.garments = [];// NOTE: hats, clothes, accessories//
+    this.background = background;
+  }
+  
+}
