f5471c15b6dc3cd874d59cf02d731125ea0b968e
Jeremy Poulter <Jpoulter@gmail.com>
2019-12-16 18:14:16 -0700

    :art: create function to make selected choice visible on bear box

--- main.js
+++ main.js
@@ -2,24 +2,40 @@ var buttonHats = document.querySelectorAll('.button-hats-js');
 var buttonClothes = document.querySelectorAll('.button-clothes-js');
 var buttonAccessories = document.querySelectorAll('.button-accessories-js');
 var buttonBackgrounds = document.querySelectorAll('.button-backgrounds-js');
+var hats = document.querySelectorAll('.hats-js');
+var clothes = document.querySelectorAll('.clothes-js');
+var accessories = document.querySelectorAll('.accessories-js');
+var backgrounds = document.querySelectorAll('.backgrounds-js');
 var globalSelector = document.querySelector('main');
 globalSelector.addEventListener('click', clickHandlerOutfits);
 
 function clickHandlerOutfits() {
   if (event.target.classList.contains('button-hats-js')) {
-    selectButton(buttonHats);
+    selectButton(buttonHats, hats);
   } else if (event.target.classList.contains('button-clothes-js')) {
-    selectButton(buttonClothes);
+    selectButton(buttonClothes, clothes);
   } else if (event.target.classList.contains('button-accessories-js')) {
-    selectButton(buttonAccessories);
+    selectButton(buttonAccessories, accessories);
   } else if (event.target.classList.contains('button-backgrounds-js')) {
-    selectButton(buttonBackgrounds);
+    selectButton(buttonBackgrounds, backgrounds);
   }
 }
 
-function selectButton(buttonType) {
+function selectButton(buttonType, choiceType) {
   for (var i = 0; i < buttonType.length; i++) {
     buttonType[i].classList.remove('selected-button');
   }
   event.target.classList.add('selected-button');
+  x = choiceType;
+  displayChoice(x);
+}
+
+function displayChoice(choiceType) {
+  console.log(choiceType);
+  for (var i = 0; i < choiceType.length; i++) {
+    choiceType[i].classList.add('hide');
+  }
+  x = event.target.value;
+  choiceType[x].classList.remove('hide');
+  console.log(choiceType[0]);
 }
